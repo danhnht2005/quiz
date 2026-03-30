44ba71bb03a2b89751862410dea01cda2576e53b
Nguyễn Huỳnh Thành Danh <thanhdanh130425@gmail.com>
2026-03-30 08:48:51 +0700

    feat: fetch API quiz

--- quiz/src/pages/Quiz/index.js
+++ quiz/src/pages/Quiz/index.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getTopic } from '../../services/topicsService';
+import { getListQuestions } from '../../services/questionsService';
+
 function Quiz() {
+  const params = useParams();
+  const [dataTopic, setDataTopic] = useState({});
+  const [dataQuestions, setDataQuestions] = useState([]);
+  
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await getTopic(params.id);
+      setDataTopic(response);
+    }
+    fetchAPI();
+  }, [params.id]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await getListQuestions(params.id);
+      setDataQuestions(response);
+    }
+    fetchAPI();
+  }, [params.id]);
+
+  console.log(dataTopic);
+  console.log(dataQuestions);
+
   return (
     <>
-      Quiz
+      <h2>Bài Quiz chủ đề: {dataTopic.name}</h2>
     </>
   );
 }
--- quiz/src/services/topicsService.js
+++ quiz/src/services/topicsService.js
@@ -4,3 +4,8 @@ export const getListTopics = async () => {
   const result = await get("topics");
   return result;
 }
+
+export const getTopic = async (id) => {
+  const result = await get(`topics/${id}`);
+  return result;
+}
