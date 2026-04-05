a2bd5cafe8460496513b9b2d99ac28f3804362dd
Nguyễn Huỳnh Thành Danh <thanhdanh130425@gmail.com>
2026-04-05 13:40:41 +0700

    feat: Complete the results page

--- quiz/src/pages/Result/index.js
+++ quiz/src/pages/Result/index.js
@@ -2,34 +2,74 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getAnswers } from "../../services/answersService";
 import { getListQuestions } from "../../services/questionsService";
+import "./result.css";
 
 function Result() {
   const param = useParams();
-  const [answers, setAnswers] = useState();
+  const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
       const dataAnswers = await getAnswers(param.id);
       const dataQuestions = await getListQuestions(dataAnswers.topicId);
-      
+
       let result = []
 
-      for(let i = 0; i < dataQuestions.length; i++) {
+      for (let i = 0; i < dataQuestions.length; i++) {
         result.push({
           ...dataQuestions[i],
           answer: dataAnswers.ansewers.find(item => item.questionId == dataQuestions[i].id)?.ansewer
         })
       }
-      console.log(result)
-    } 
+      setAnswers(result);
+    }
     fetchAPI();
   }, [])
 
+  console.log(answers);
+
   return (
     <>
-      
+      <h2>Kết quả</h2>
+
+      <div className="result__list">
+        {answers.map((item, index) => (
+          <div className='result__list__item' key={index}>
+            <p>
+              Câu {index + 1}: {item.question}
+              {item.correctAnswer === item.answer ? (
+                <span className="result__tag result__tap--true">Đúng</span>
+              ) : (
+                <span className="result__tag result__tap--false">Sai</span>
+              )}
+            </p>
+
+            {item.answers.map((itemAns, indexAns) => {
+              let className = "";
+              let checked  = false;
+
+              if (item.answer === indexAns) {
+                checked = true;
+                className = "result__item--selected";
+              }
+
+              if (item.correctAnswer === indexAns) {
+                className += " result__item--result"
+              }
+
+              return (
+                <div className='result__item--answers' key={indexAns}>
+                  <input type='radio' checked={checked} disabled />
+                  <label className={className}>{itemAns}</label>
+                </div>
+              )
+            })}
+
+          </div>
+        ))}
+      </div>
     </>
-  );  
+  );
 }
 
 export default Result;
